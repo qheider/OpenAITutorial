d700e60ed4d41e47267689e68f49dbdc43ee49ee
Quazi Heider <qheider@gmail.com>
2025-11-09 12:25:45 -0500

    set instruction to input array

--- example3.py
+++ example3.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Initialize client with API key
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-
+# Using instruction in the response creation
 response = client.responses.create(
     model="gpt-5-nano",
     reasoning={"effort": "low"},
@@ -17,3 +17,18 @@ response = client.responses.create(
 )
 print(response.output_text)
 
+#put instruction inside input array as Developer role and user question as User role
+response = client.responses.create(
+    model="gpt-5-nano",
+    reasoning={"effort": "low"},
+    input=[
+ {
+            "role": "developer",
+            "content": "Talk like a pirate."
+        },
+{"role": "user", 
+ "content": "Are semicolons optional in JavaScript?"}
+
+    ]
+)   
+print(response.output_text)
